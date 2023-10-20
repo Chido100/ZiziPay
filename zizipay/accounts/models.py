b9fbe02834cd48604a51a0a9922e73c3656a6679
Chidozie <109086527+Chido100@users.noreply.github.com>
2023-10-20 15:43:21 +0100

    Account model modification

--- zizipay/accounts/models.py
+++ zizipay/accounts/models.py
@@ -4,45 +4,45 @@ from shortuuid.django_fields import ShortUUIDField
 from users.models import User
 
 
-
-ACCOUNT_STATUS = (
-    ('active', 'Active'),
-    ('in-active', 'In-active')
-)
-
-MARITAL_STATUS = (
-    ('simgle', 'Single'),
-    ('married', 'Married'),
-    ('other', 'Other')
-)
-
-GENDER = (
-    ('male', 'Male'),
-    ('female', 'Female'),
-    ('other', 'Other')
-)
-
-IDENTITY_TYPE = (
-    ('international_passport', 'International Passport'),
-    ('drivers_licence', 'Drivers Licence')
-)
-
-
+# Uploaded files storage
 def user_directory_path(instance, filename):
     ext = filename.split(".")[-1]
     filename = "%s_%s" % (instance.id, ext)
     return "user_{0}/{1}".format(instance.user.id, filename)
 
 
+ACCOUNT_STATUS = (
+    ('active', 'Active'),
+    ('in-active', 'In-active'),
+)
+
+MARITAL_STATUS = (
+    ('simgle', 'Single'),
+    ('married', 'Married'),
+    ('other', 'Other'),
+)
+
+GENDER = (
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other'),
+)
+
+IDENTITY_TYPE = (
+    ('international_passport', 'International Passport'),
+    ('drivers_licence', 'Drivers Licence'),
+)
+
+
 class Account(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, defualt=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     account_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     account_number = ShortUUIDField(unique=True, length=10, max_length=25, prefix='119', alphabet='1234567890')
     account_id = ShortUUIDField(unique=True, length=7, max_length=25, prefix='ZIZ', alphabet='1234567890')
     pin_number = ShortUUIDField(unique=True, length=4, max_length=7, alphabet='1234567890')
-    ref_code = ShortUUIDField(unique=True, length=10, max_length=7, alphabet='abcdefgh1234567890')
-    account_status = models.CharField(max_length=100, choices=ACCOUNT_STATUS, default='In-active')
+    ref_code = ShortUUIDField(unique=True, length=7, max_length=7, alphabet='abcdefgh1234567890')
+    account_status = models.CharField(max_length=100, choices=ACCOUNT_STATUS, default='in-active')
     date = models.DateTimeField(auto_now_add=True)
     kyc_submitted = models.BooleanField(default=False)
     kyc_confirmed = models.BooleanField(default=False)
@@ -52,4 +52,4 @@ class Account(models.Model):
         ordering = ['-date']
 
     def __str__(self):
-        return self.user
+        return f'{self.user}'
